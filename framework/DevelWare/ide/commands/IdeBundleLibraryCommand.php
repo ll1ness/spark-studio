9cc91af1b43e3d06aaa49ac27a00ce302a94e9f4
ll1ness <roness@ro.ru>
2026-05-17 12:48:56 +0300

    Fix IdeBundleLibraryCommand: implement onExecute method with open bundles directory

--- framework/DevelWare/ide/commands/IdeBundleLibraryCommand.php
+++ framework/DevelWare/ide/commands/IdeBundleLibraryCommand.php
@@ -1,12 +1,25 @@
 <?php
 namespace ide\commands;
 
+use ide\editors\AbstractEditor;
+use ide\Ide;
 use ide\misc\AbstractCommand;
+use php\gui\UXDesktop;
+use php\io\File;
 
 class IdeBundleLibraryCommand extends AbstractCommand
 {
     public function getName()
     {
         return 'Пакеты расширений';
+    }
+
+    public function onExecute($e = null, AbstractEditor $editor = null)
+    {
+        $dir = Ide::get()->getLibrary()->getResourceDirectory('bundles');
+        if ($dir) {
+            $desktop = new UXDesktop();
+            $desktop->open(File::of($dir));
+        }
     }
 }
